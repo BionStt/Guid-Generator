d5c624e5f11f8d535691449997c37cba01838b15
Edi Wang <Edi.Wang@outlook.com>
2021-06-24 14:07:48 +0800

    basic logic

---
+++ src/Pages/Index.razor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace GuidGenBlazor.Pages
+{
+    public partial class Index
+    {
+        [Inject]
+        public IJSRuntime JavaScriptRuntime { get; set; }
+
+        public string Result { get; set; }
+
+        private void NewGuid()
+        {
+            Result = Guid.NewGuid().ToString();
+        }
+
+        private async Task CopyResult()
+        {
+            if (!string.IsNullOrWhiteSpace(Result))
+            {
+                await JavaScriptRuntime.InvokeVoidAsync("clipboardCopy.copyText", Result);
+            }
+        }
+    }
+}
